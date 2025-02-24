088b136792744b465e8276f7f7564304a4438802
Ravindra Singh <ravindrasingh@Mac.lan>
2025-02-24 08:40:06 +0530

    learn dynamic metadata

--- learning-next-js/src/app/blog/[blogId]/page.tsx
+++ learning-next-js/src/app/blog/[blogId]/page.tsx
@@ -1,8 +1,17 @@
-export default async function BlogDetail({
-  params,
-}: {
+import { Metadata } from "next";
+type props = {
   params: { blogId: string };
-}) {
+};
+
+//dynamic metadata
+export const generateMetadata = ({ params }: props): Metadata => {
+  return {
+    title: `product ${params.blogId}`,
+    description: `generate by ${params.blogId}`,
+  };
+};
+
+export default async function BlogDetail({ params }: props) {
   const { blogId } = await params;
   return <div>Blog Detail: {blogId}</div>;
 }
